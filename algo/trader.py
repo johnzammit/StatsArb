924566ee862f8f266f075520b2626c56d4db65c2
TJ Pavaritpong <teetajp@gmail.com>
2023-10-14 14:11:51 -0500

    add placeholder files for project structure

---
+++ algo/trader.py
@@ -0,0 +1,11 @@
+from typing import Dict, List
+from datamodel import OrderDepth, TradingState, Order
+
+
+class Trader:
+
+    def __init__(self):
+        pass
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        pass
