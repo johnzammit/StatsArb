7af7d4492d1354ce99bb7f982a67c25bde3c61ee
TJ Pavaritpong <teetajp@gmail.com>
2023-10-14 14:11:51 -0500

    add placeholder files for project structure

---
+++ algo/datamodel.py
@@ -0,0 +1,16 @@
+import typing
+from typing import Dict, List
+from json import JSONEncoder
+
+class Listing:
+    pass
+class Order:
+    pass
+
+class OrderDepth:
+    pass
+class Trade:
+    pass
+
+class TradingState(object):
+    pass
