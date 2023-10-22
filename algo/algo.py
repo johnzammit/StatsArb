4f9a440bc88f4476126b0468cfca6ccd4dc8b9ec
TJ Pavaritpong <teetajp@gmail.com>
2023-10-21 21:56:39 -0500

    merge algo.py

--- algo/algo.py
+++ algo/algo.py
@@ -5,88 +5,179 @@
 ##Create buy and sell functions
 ##price = bianace.get_price('BTCUSDT')
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+import json
+import datetime
+import math
 from market_state import MarketState
+
 
 """
 Execution will using MarketState data to know when to execute a trade.
 """
+import urllib.parse
+import hashlib
+import hmac
+import base64
+import requests
+import time
 
 
-class Executor:
-    def __init__(self, client: Client, coin1: tuple[str, float], coin2: tuple[str, float]):
-        # coin1 and coin2 are tuples containing (ticker, weight)
-        self.coin1 = coin1
-        self.coin2 = coin2
-        self.client = client
+class Execution():
+    def __init__(self, formula, api_key, secret_key, api_url):
+        """"Formula is a tuple containing coins and their weights (ticker, weight)"""
+        # need to take in a formula
+        self.api_url = "https://api.binance.us"
+        self.api_key = "YOUR_API_KEY"
+        self.secret_key = "YOUR_SECRET_KEY"
+
+    # get binanceus signature
+    def get_binanceus_signature(self, data, secret):
+        postdata = urllib.parse.urlencode(data)
+        message = postdata.encode()
+        byte_key = bytes(secret, 'UTF-8')
+        mac = hmac.new(byte_key, message, hashlib.sha256).hexdigest()
+        return mac
+
+    # Attaches auth headers and returns results of a POST request
+    def binanceus_request(self, uri_path, data, api_key, api_sec):
+        headers = {}
+        headers['X-MBX-APIKEY'] = api_key
+        signature = self.get_binanceus_signature(data, api_sec)
+        payload = {
+            **data,
+            "signature": signature,
+        }
+        req = requests.post((self.api_url + uri_path), headers=headers, data=payload)
+        return req.text
 
     # checks if the spread is above the upper bollinger band or below the lower bollinger band
     async def place_order_condition(self, spread: float, upper_bollinger_band_price: float,
-                                    lower_bollinger_band_price: float, higher_coin: str, lower_coin: str) -> bool:
+                                    lower_bollinger_band_price: float) -> bool:
         return spread >= upper_bollinger_band_price or spread <= lower_bollinger_band_price
 
-    async def sell_order_position(self):
-        pass
+        # tell binance to buy at a certain price
 
-    # tell binance to buy at a certain price
-    async def place_limit_buy(self, coin, price):
-        pass
+    async def place_limit_buy(self, symbol: str, price: float, quantity: float, stopPrice: float):
+        uri_path = "/api/v3/order"
+        data = {
+            "symbol": symbol,
+            "side": "BUY",
+            "type": "LIMIT",
+            "price": price,
+            "quantity": quantity,
+            "timeinforce": "GTC",  # good till cancelled
+            "timestamp": int(round(time.time() * 1000))
+        }
+
+        result = self.binanceus_request(uri_path, data, self.api_key, self.secret_key)
+        print("POST {}: {}".format(uri_path, result))
 
     # tell binance to sell at a certain price
-    async def place_limit_sell(self, coin, price):
-        pass
+    async def place_limit_sell(self, symbol: str, price: float, quantity: float, stopPrice: float):
+        uri_path = "/api/v3/order"
+        data = {
+            "symbol": symbol,  # ex. BTCUSDT
+            "side": "SELL",
+            "type": "LIMIT",
+            "price": price,
+            "quantity": quantity,
+            "stopPrice": stopPrice,  # price at which limit order is triggered
+            "timeinforce": "GTC",  # good till cancelled
+            "timestamp": int(round(time.time() * 1000))
+        }
+
+        result = self.binanceus_request(uri_path, data, self.api_key, self.secret_key)
+        print("POST {}: {}".format(uri_path, result))
 
     # cancel unfilled limit sell orders and place new ones at updated price
-    async def update_limit_sell(self, lower_coin, take_profit_price):
-        pass
+    async def update_limit_sell(self, short_coin, take_profit_price, cancelReplaceMode, cancelOrderId, timeInForce,
+                                quantity: float, price: float):
+        uri_path = "/api/v3/order/cancelReplace"
+        data = {
+            "timestamp": int(round(time.time() * 1000)),
+            "symbol": short_coin,
+            "side": "SELL",
+            "type": "LIMIT",
+            "cancelReplaceMode": cancelReplaceMode,
+            "cancelOrderId": cancelOrderId,
+            "timeInForce": timeInForce,
+            "quantity": quantity,
+            "price": price
+        }
+
+        result = Execution.binanceus_request(uri_path, data, self.api_key, self.secret_key)
+        print("POST {}: {}".format(uri_path, result))
 
     # check if orders are still open or not before placing new orders
-    async def check_order_status(self):
-        # cur_orders = self.client.get_open_orders((self.coin1[0], self.coin2[0]))
-        # return len(cur_order) == 0
-        pass
+    async def check_open_orders(self) -> bool:
+        uri_path = "/api/v3/openOrders"
+        data = {
+            "timestamp": int(round(time.time() * 1000))
+        }
+        result = self.binanceus_request(uri_path, data, self.api_key, self.secret_key)
+        response_data = result.json()
+        # Check if there are open orders
+        if len(response_data) > 0:
+            return True
+        else:
+            return False
 
-    # calculate time weighted take profit price
-    async def take_profit_price(self, coin: float) -> float:
+    # calculate time weighted take profit price for long position
+    async def take_profit_long_price(self, coin: float, timestamp) -> float:
         decrease_rate = 0.055
         target_price = MarketState.spread_moving_avg() - (
-                    decrease_rate * t) + MarketState.derivative_of_spread() % MarketState.current_price(coin) / 100
+                decrease_rate * timestamp) + MarketState.derivative_of_spread() % MarketState.current_price(
+            coin) / 100
         ## Need to add accelerator with the derivative of spread
 
         return target_price
 
-    # TODO: either make this executor call the market every tick
-    #  or use a while loop outside that, for every tick, gets the MarketState and calls executor.run
-    #  and sends a response back to the exchange based on Executor.run()'s return val (this makes debugging easier)
-    def run(self, market: MarketState):
-        # check if we should buy or sell
-        if (place_order_condition()):
-            place_limit_buy(higher_coin, price)
-            place_limit_sell(lower_coin, price)
-            # start timer
-            time = 0
-            # monitor when to sell
-            while (value of all positions > hard_stop_loss()):
-                # update take profit price
-                take_profit_price()
+    # calculate time weighted take profit price for short position
+    async def take_profit_short_price(self, coin: float, timestamp) -> float:
+        decrease_rate = 0.055
+        target_price = MarketState.spread_moving_avg() + (
+                decrease_rate * timestamp) + MarketState.derivative_of_spread() % MarketState.current_price(
+            coin) / 100
+        ## Need to add accelerator with the derivative of spread
 
-                # place/update limit sell order at take profit price
-                if (no limit sell orders open):
-                    place_limit_sell(lower_coin, take_profit_price)
-                    place_limit_sell(higher_coin, take_profit_price)
-                else:
-                    update_limit_sell(lower_coin, take_profit_price)
-                    update_limit_sell(higher_coin, take_profit_price)
+        return target_price
+
+    def main(self, market: MarketState):
+        # Define all variables using MarketState
+
+        # check if we should buy or sell
+        if (self.place_order_condition(spread, upper_bollinger_band_price, lower_bollinger_band_price)):
+            self.place_limit_buy(long_coin, price, quantity, stopPrice)
+            self.place_limit_sell(symbol, price, quantity, stopPrice)
+
+            # start timer
+            timestamp = int(time.time() * 1000)
+
+            # monitor when to sell
+            while (self.check_open_orders()):
+                # update variables using MarketState
+
+                # check if price is at or below hard stop loss
+                if (MarketState.current_price(long_coin) <= MarketState.hard_stop_loss(
+                        long_coin) or MarketState.current_price(short_coin) >= MarketState.hard_stop_loss(short_coin)):
+                    # sell at hard stop loss
+                    self.place_limit_buy(long_coin, price, quantity, stopPrice)
+                    self.place_limit_sell(symbol, price, quantity, stopPrice)
+                # update take profit price
+                self.take_profit_long_price(long_coin, timestamp)
+                self.take_profit_short_price(short_coin, timestamp)
+
+                # consider adding a time delay here
+
+                # update limit sell order at take profit price
+                self.update_limit_sell(long_coin, take_profit_price)
+                self.update_limit_sell(short_coin, take_profit_price)
 
                 # check if orders are still open
-                if check_order_status():
+                if self.check_open_orders():
                     continue
                 else:
-                    break
-            # sell at hard stop loss
-            place_limit_sell(lower_coin, price)
-            place_limit_sell(higher_coin, price)
-
-        pass
+                    breaks
 
 
 if __name__ == '__main__':
@@ -94,7 +185,7 @@ if __name__ == '__main__':
     # TODO: determine how to synchronize executor to market caller
 
     # TODO: use multithreading?
-    executor1 = Executor( ('DOGEUSDT', 1), ('MASKUSDT', -1))
-    executor2 = Executor( ('BNBUSDT', 1), ('DOGEUSDT', -1))
+    # executor = Executor(None, None, None, None)
 
+    # executor.main()
     pass
