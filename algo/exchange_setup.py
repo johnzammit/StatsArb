199afd6545e2abf9c0850fb354d60c0072ef8568
Jimmy Huang <jhuan35@illinois.edu>
2023-11-09 20:52:52 -0600

    Using new function get_data to get historical data and saving it in MarketState. Maintaing a row count variable in MarketState

--- algo/exchange_setup.py
+++ algo/exchange_setup.py
@@ -7,5 +7,5 @@ def establish_connection(useProd: bool = False) -> Client:
     tld = 'us' if useProd else 'com'
     binance_api_key = os.getenv('binanceAPIKey') if useProd else os.getenv('binanceAPIKey_testnet')
     binance_secret_key = os.getenv('binanceAPIKey') if useProd else os.getenv('binanceSecretKey_testnet')
-    assert(binance_api_key and binance_secret_key)
+    # assert(binance_api_key and binance_secret_key)
     return Client(binance_api_key, binance_secret_key, tld=tld, testnet=(not useProd))
